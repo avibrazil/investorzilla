dac9d1d0f0836a986d85428457aebe5bad621405
Avi Alkalay <avibrazil@gmail.com>
2025-04-02 16:48:00 -0300

    updated BCB benchmarks to support indexes that require paging

--- investorzilla/marketindex/brasil_banco_central.py
+++ investorzilla/marketindex/brasil_banco_central.py
@@ -1,3 +1,5 @@
+import urllib
+import concurrent.futures
 import pandas
 
 from .. import MarketIndex
@@ -9,29 +11,34 @@ class BCBMarketIndex(MarketIndex):
     series={
         'SELIC': dict(
             url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json",
-            home = 'https://dadosabertos.bcb.gov.br/dataset/11-taxa-de-juros---selic'
+            home = 'https://dadosabertos.bcb.gov.br/dataset/11-taxa-de-juros---selic',
+            paged = True
         ),
         'CDI': dict(
             url="https://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados?formato=json",
-            home='https://www3.bcb.gov.br/sgspub/consultarmetadados/consultarMetadadosSeries.do?method=consultarMetadadosSeriesInternet&hdOidSerieSelecionada=12'
+            home='https://www3.bcb.gov.br/sgspub/consultarmetadados/consultarMetadadosSeries.do?method=consultarMetadadosSeriesInternet&hdOidSerieSelecionada=12',
+            paged = True
         ),
         'IPCA': dict(
             # IPCA Serviços. Outros IPCAs: https://dadosabertos.bcb.gov.br/dataset?q=ipca
             url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json",
-            home = 'https://dadosabertos.bcb.gov.br/dataset/10844-indice-de-precos-ao-consumidor-amplo-ipca---servicos'
+            home = 'https://dadosabertos.bcb.gov.br/dataset/10844-indice-de-precos-ao-consumidor-amplo-ipca---servicos',
+            paged = False
         ),
         'IGPM': dict(
             # url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.4175/dados?formato=json",
             url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.189/dados?formato=json",
-            home = 'https://dadosabertos.bcb.gov.br/dataset/4175-divida-mobiliaria---participacao-por-indexador---posicao-em-carteira---igp-m'
+            home = 'https://dadosabertos.bcb.gov.br/dataset/4175-divida-mobiliaria---participacao-por-indexador---posicao-em-carteira---igp-m',
+            paged = False
         ),
         'INPC': dict(
             url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.188/dados?formato=json",
-            home = 'https://www3.bcb.gov.br/sgspub/consultarmetadados/consultarMetadadosSeries.do?method=consultarMetadadosSeriesInternet&hdOidSerieSelecionada=188'
+            home = 'https://www3.bcb.gov.br/sgspub/consultarmetadados/consultarMetadadosSeries.do?method=consultarMetadadosSeriesInternet&hdOidSerieSelecionada=188',
+            paged = False
         ),
     }
 
-
+    paged_period_params='&dataInicial={start:%d/%m/%Y}&dataFinal={end:%d/%m/%Y}'
 
     def __init__(self, name, isRate=True, cache=None, refresh=False):
         if name in self.series:
@@ -50,11 +57,77 @@ class BCBMarketIndex(MarketIndex):
 
 
     def refreshData(self):
-        try:
-            self.data=pandas.read_json(self.series[self.id]['url'])
-        except BaseException as err:
-            self.logger.warning(f"URL was: {self.series[self.id]['url']}")
-            raise
+        if self.series[self.id]['paged']:
+            # Generate periods of 5 years from today until 1980
+            periods = pandas.date_range(
+                start=pandas.Timestamp.today() + pandas.Timedelta(days=1),
+                end=pandas.Timestamp('1980-01-01'),
+                freq='-60ME'
+            )
+
+            # Read URL content for multiple periods in parallel
+            with concurrent.futures.ThreadPoolExecutor(thread_name_prefix='load_domains') as executor:
+                tasks=dict()
+                self.data=None
+                for i in range(len(periods)):
+                    if i+1==len(periods):
+                        break
+                    end=periods[i]
+                    start=periods[i+1]
+                    # display(url.format(start=start,end=end))
+                    url=(
+                        (self.series[self.id]['url']+self.paged_period_params)
+                        .format(
+                            start=start,
+                            end=end
+                        )
+                    )
+
+                    # Read URL in background
+                    task=executor.submit(
+                        # Method to execute in background
+                        pandas.read_json,
+
+                        # Parameters to the method: the index URL
+                        url
+                    )
+
+                    tasks[task]=(self.id,start,end)
+
+                for task in concurrent.futures.as_completed(tasks):
+                    try:
+                        if self.data is None:
+                            self.data=task.result()
+                        else:
+                            self.data=pandas.concat([self.data,task.result()])
+                    except urllib.error.HTTPError as e:
+                        self.logger.warning(f"Failed to retrieve {tasks[task][0]} for period {tasks[task][1]} → {tasks[task][2]}. Probably data series has no data for period.")
+
+            # Concatenate all results together and do minimum processing
+            self.data=(
+                self.data
+                .assign(
+                    time=lambda table: (
+                        (
+                            # Convert to datetime
+                            pandas.to_datetime(table.data,dayfirst=True) +
+
+                            # This is date-only information but we know this is the
+                            # end of the day
+                            pandas.Timedelta(hours=22, minutes=59)
+                        )
+                    ),
+                )
+                .sort_values('time')
+                .drop('time',axis=1)
+                .drop_duplicates()
+            )
+        else:
+            try:
+                self.data=pandas.read_json(self.series[self.id]['url'])
+            except urllib.URLError as err:
+                self.logger.warning(f"URL was: {self.series[self.id]['url']}")
+                raise
 
 
 
